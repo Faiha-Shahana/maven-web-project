13c7d44af8782bf838ff69df6435f6101902b61f
Ashiqjr <2000414i@student.tp.edu.sg>
2022-02-05 19:18:51 +0800

    lab till 14 part2

---
+++ src/main/java/employeedetails.java
@@ -0,0 +1,44 @@
+
+public class employeedetails {
+	protected String employee_name;
+	 protected String employee_phone;
+	 protected String employee_pay;
+	 protected String salary_status;
+	 
+	 public  employeedetails(String name, String phone, String pay, String salary) {
+	 	super();
+	 	this.employee_name=name;
+	 	this.employee_phone= phone;
+	 	this.employee_pay= pay;
+	 	this.salary_status= salary;
+}	 	
+	 	
+	 	
+	 
+	public String getEmployee_name() {
+		return employee_name;
+	}
+	public void setEmployee_name(String employee_name) {
+		this.employee_name = employee_name;
+	}
+	public String getEmployee_phone() {
+		return employee_phone;
+	}
+	public void setEmployee_phone(String employee_phone) {
+		this.employee_phone = employee_phone;
+	}
+	public String getEmplyee_pay() {
+		return emplyee_pay;
+	}
+	public void setEmplyee_pay(String emplyee_pay) {
+		this.emplyee_pay = emplyee_pay;
+	}
+	public String getSalary_status() {
+		return salary_status;
+	}
+	public void setSalary_status(String salary_status) {
+		this.salary_status = salary_status;
+	}
+	 
+
+}
---
+++ src/main/webapp/register.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Revenue Tracker</title>
+</head>
+<body>
+<form action="RegisterServlet" method="post">
+Name: <input type="text" name="employee_name">
+Phone: <input type="text" name="employee_phone">
+Pay: <input type="text" name="employee_pay">
+Status: <select name="salary_status">
+<option>Paid</option>
+<option>Still Pending</option>
+</select>
+<input type="submit" value="Update Employee" />
+
+
+
+</form>
+</body>
+</html>
